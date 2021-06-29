f977e5bee92ed861ff831b9d172e9cb53c560d69
jssgajendrasingh <mailmegajendra14@gmail.com>
2021-06-29 17:22:19 +0530

    adding relationship

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 	belongs_to :user
 	has_many :comments
-	has_and_belongs_to_many :parts
-
+	has_many :article_tags
+    has_many :tags, through: :article_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
-		has_and_belongs_to_many :articles
+	has_many :article_tags
+	has_many :articles,through: :article_tags
 end
