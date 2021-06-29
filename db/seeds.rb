bb95eca2a9ff6615b7a0fbcb11f753ffd5ec556f
jssgajendrasingh <mailmegajendra14@gmail.com>
2021-06-29 17:52:34 +0530

    creating user and article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#10.times do
+#	user = User.create(firstname: "gajendra",lastname: "singh",about: "mca",email: "mailmegajendra14@gmail.com",password: "Gajendra@21")
+#end
+
+1.upto(10) do |i|
+	article = Article.create(title: "ruby on rails",content: "seeding documention",is_publish: false,user_id: i)
+end	
